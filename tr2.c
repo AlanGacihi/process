73025bcabc6cf4092bc54883b33b664da8dce8d0
AlanGacihi <alanmamicha@gmail.com>
2022-10-10 14:31:42 +0300

    aDD

--- tr2.c
+++ tr2.c
@@ -5,19 +5,20 @@
  
 // A normal C function that is executed as a thread
 // when its name is specified in pthread_create()
-void *myThreadFun(void *vargp)
+void *ThreadFun(void *vargp)
 {
-    sleep(1);
-    printf("Printing GeeksQuiz from Thread \n");
-    return NULL;
+    char *filename;
+    filename = (char *)vargp;
+    printf("Printing GeeksQuiz from Thread %s\n", filename);
+    pthread_exit(NULL);
 }
   
 int main()
 {
     pthread_t thread_id;
     printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, myThreadFun, NULL);
-    pthread_join(thread_id, NULL);
+    pthread_create(&thread_id, NULL, ThreadFun, (void *)"Hello");
+    //pthread_join(thread_id, NULL);
     printf("After Thread\n");
     exit(0);
 }
