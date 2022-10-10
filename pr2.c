b8265d4bb44aff1af99918a48c119734e22a0758
AlanGacihi <alanmamicha@gmail.com>
2022-10-10 12:14:55 +0300

    Add

--- pr2.c
+++ pr2.c
@@ -72,6 +72,7 @@ int main(void)
 
                 /* Send data to the main pipe */
                 sprintf(buffer, "%f %f %f %f\n", min + max, min - max, min, max);
+                printf("%s", buffer);
                 write(fd[1], buffer, (strlen(buffer)+1));
                 exit(0);
             }
